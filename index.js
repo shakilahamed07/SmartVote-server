aadda388710b3ff2d3a9c0020d77f76cc47c8ff1
shakilahamed07 <shakil200607@gmail.com>
2026-04-07 09:09:36 +0600

    setup server

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const prot = process.env.prot || 5000;
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.DB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const VotingEvent = client.db("smart-vote").collection("votingEvent");
+    const UserRegistration = client.db("smart-vote").collection("userRegistration");
+    const ValidRegistration = client.db("smart-vote").collection("validRegistration");
+    const AllCandidates = client.db("smart-vote").collection("allCandidates");
+
+    
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+
+app.listen(prot, () => {
+  console.log(`server is running prot${prot}`);
+});
